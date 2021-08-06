f1abe21171973bc50ae9ed0ad67ec3769f51872f
HuaKang Huang <wb-hhk798061@alibaba-inc.com>
2021-08-06 20:02:32 +0800

    获取文件根目录

--- node实战/demo_02/controller/controller.js
+++ node实战/demo_02/controller/controller.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const path = require("path");
 const appController = (server) => {
   server.all("*", (req, res, next) => {
     //设为指定的域
@@ -12,13 +13,51 @@ const appController = (server) => {
   });
   server.post("/readTargetFile.json", (req, res) => {
     const { fileUrl } = req.body;
+    const _targetObj = {};
+    const fileUrlArr = fileUrl.split(path.sep);
+    const fileName = fileUrlArr[fileUrlArr.length - 1];
+    _targetObj[`${fileName}`] = {};
     fs.readdir(`${fileUrl}`, (err, file) => {
-      console.log(file);
-      // file.forEach((_item) => {
-      //   fs.readFile(`${fileUrl}/${_item}`, (err, data) => {
-      //     res.json({ fileContext: data.toString() });
-      //   });
-      // });
+      file.forEach((_item, index) => {
+        if (path.extname(_item)) {
+          //如果是文件，就读取文件内容
+          fs.readFile(`${fileUrl}/${_item}`, (err, data) => {
+            _targetObj[`${fileName}`][`${_item}`] = data.toString();
+          });
+        } else {
+          //这里是文件夹，继续读取文件夹，直到获取到文件
+          _targetObj[`${fileName}`][`${_item}`] = {};
+          setTimeout(() => {
+            fs.readdir(`${fileUrl}/${_item}`, (err, data) => {
+              if (data && data.length > 0) {
+                data.forEach((_childItem) => {
+                  if (path.extname(_childItem)) {
+                    //如果是文件，就读取文件内容
+                    fs.readFile(
+                      `${fileUrl}/${_item}/${_childItem}`,
+                      (err, _childData) => {
+                        _targetObj[`${fileName}`][`${_item}`][`${_childItem}`] =
+                          _childData.toString();
+                      }
+                    );
+                  } else {
+                    _targetObj[`${fileName}`][`${_item}`][`${_childItem}`] = {};
+                  }
+                });
+              } else {
+                _targetObj[`${fileName}`][`${_item}`] = {};
+              }
+            });
+          }, 1000);
+        }
+      });
+      setTimeout(() => {
+        res.json({
+          message: "请求成功！",
+          success: true,
+          data: _targetObj,
+        });
+      }, 2000);
     });
   });
 };
--- saveLocalProject/src/page/uploadCode/index.tsx
+++ saveLocalProject/src/page/uploadCode/index.tsx
@@ -1,10 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import Request from '../../../utils/http'
 
-import { Button,Input } from 'antd';
+import { Button,Input,Menu,Layout } from 'antd';
 
+const { SubMenu } = Menu;
+const { Header, Content, Sider } = Layout;
 const UploadCode=():React.ReactElement=>{
   const [fileUrl,setFileUrl]=useState('');
+  const [fileDir,setFileDir]=useState({});
   return <React.Fragment>
       <Input placeholder="请输入需要上传的文件路径" onChange={(val)=>{
         console.log(val.target.value);
@@ -13,12 +16,50 @@ const UploadCode=():React.ReactElement=>{
       <Button type="primary" onClick={async()=>{
       Request.post("/readTargetFile.json",{fileUrl:fileUrl}).then(res=>{
         console.log(res);
+        setFileDir(res.data)
       })
         
         // axios.post('http://localhost:3333/readTargetFile.json',{url:fileUrl})
         // http://localhost:3333/readTargetFile.json
 
       }}>上传</Button>
+      <Sider width={200} className="site-layout-background">
+      <Menu
+        mode="inline"
+        defaultSelectedKeys={['1']}
+        defaultOpenKeys={['sub1']}
+        style={{ height: '100%', borderRight: 0 }}
+      >
+        {
+          Object.keys(fileDir).map(_item=>{
+            return <SubMenu key={_item} title={_item}>
+                        {
+                          fileDir[`${_item}`]&&Object.keys(fileDir[`${_item}`]).map(_childItem=>{
+                            if(typeof(fileDir[`${_item}`][`${_childItem}`])!=="string"){
+                              return <SubMenu key={_childItem} title={_childItem}>
+                                {
+                                  Object.keys(fileDir[`${_item}`][`${_childItem}`]).length>0&&Object.keys(fileDir[`${_item}`][`${_childItem}`]).map(childChildItem=>{
+                                      return <Menu.Item key={childChildItem+"/child"}>{childChildItem}</Menu.Item>
+                                  })
+                                  // if(typeof(fileDir[`${_item}`][`${_childItem}`])!=="string"){
+                                  //   return <SubMenu key={_childItem} title={_childItem}></SubMenu>
+                                  // }else{
+                                  //   return <Menu.Item key={_childItem+"/child"}>{_childItem}</Menu.Item>
+                                  // }
+                                }
+                              </SubMenu>
+                            }else{
+                              return <Menu.Item key={_childItem+"/child"}>{_childItem}</Menu.Item>
+                            }
+                          })
+                        }
+                      {/*  */}
+                  </SubMenu>
+          })
+        }
+      </Menu>
+    </Sider>
+      
   </React.Fragment>
 }
 export default UploadCode;
